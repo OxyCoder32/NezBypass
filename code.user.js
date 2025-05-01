be2d5fcf4061c106ba4b6bc3e44f6ee8e18afbae
OxyCoder <168300911+perritoelpro32@users.noreply.github.com>
2025-05-01 11:46:36 -0500

    Update code.user.js

--- code.user.js
+++ code.user.js
@@ -1,10 +1,99 @@
 // ==UserScript==
-// @name         Nez API Key Verification & Script Execution
+// @name         Nez Userscript
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Verify API Key and execute Main.js script if valid
 // @author       OxyCoder
-// @match        *://*/*
+// @match        *://linkvertise.com/*
+// @match        *://loot-link.com/*
+// @match        *://loot-links.com/*
+// @match        *://lootlink.org/*
+// @match        *://lootlinks.co/*
+// @match        *://lootdest.info/*
+// @match        *://lootdest.org/*
+// @match        *://lootdest.com/*
+// @match        *://links-loot.com/*
+// @match        *://linksloot.net/*
+// @match        *://*.onsultingco.com/*
+// @match        *://sub2unlock.com/*
+// @match        *://sub2unlock.io/*
+// @match        *://sub2unlock.net/*
+// @match        *://sub2unlock.me/*
+// @match        *://sub2unlock.online/*
+// @match        *://sub2unlock.top/*
+// @match        *://sub4unlock.pro/*
+// @match        *://sub4unlock.com/*
+// @match        *://sub4unlock.io/*
+// @match        *://links.spacebin.in/*
+// @match        *://adfoc.us/*
+// @match        *://boost.ink/*
+// @match        *://cuty.io/*
+// @match        *://www.cuty.io/*
+// @match        *://cutyion.com/*
+// @match        *://cety.app/*
+// @match        *://www.cety.app/*
+// @match        *://mboost.me/*
+// @match        *://bst.gg/*
+// @match        *://booo.st/*
+// @match        *://bstlar.com/*
+// @match        *://mendationforc.info/*
+// @match        *://paster.so/*
+// @match        *://paster.gg/*
+// @match        *://rekonise.com/*
+// @match        *://social-unlock.com/*
+// @match        *://www.social-unlock.com/*
+// @match        *://socialwolvez.com/*
+// @match        *://sub2get.com/*
+// @match        *://www.sub2get.com/*
+// @match        *://work.ink/*
+// @match        *://workink.net/*
+// @match        *://paste.work.ink/view.html*
+// @match        *://outgoing.work.ink/*
+// @match        *://auth.platoboost.com/*
+// @match        *://auth.platoboost.net/*
+// @match        *://auth.platorelay.com/*
+// @match        *://auth.platorelay.net/*
+// @match        *://spdmteam.com/*
+// @match        *://mobile.codex.lol/*
+// @match        *://flux.li/*
+// @match        *://keyguardian.org/a/*
+// @match        *://keyrblx.com/getkey/*
+// @match        *://pandadevelopment.net/*
+// @match        *://krnl.cat/*
+// @match        *://www.nixius.xyz/*
+// @match        *://trigonevo.fun/*
+// @match        *://arc-hub.xyz/*
+// @match        *://alchemyhub.xyz/key*
+// @match        *://ads.luarmor.net/get_key?*
+// @match        *://hehehub-acsu123.pythonanywhere.com/api/getkeyv4*
+// @match        *://hohocomunity.xyz/api/getkeyv4*
+// @match        *://hohocomunity.xyz/api/getkeyv2*
+// @match        *://ldnesfspublic.org/https*
+// @match        *://thebasement.ink/*
+// @match        *://apixerohub.x10.mx/api/getkey?*
+// @match        *://getkey.farrghii.com/*
+// @match        *://overdrivehub.xyz/*
+// @match        *://auth.btteam.net/*
+// @match        *://stfly.biz/*
+// @match        *://shrtlk.click/*
+// @match        *://airevue.net/*
+// @match        *://atravan.net/*
+// @match        *://*/recaptcha/*
+// @match        *://pastebin.com/*
+// @match        *://paste-drop.com/*
+// @match        *://pastefy.app/*
+// @match        *://esohasl.net/*
+// @match        *://linkunlocker.com/*
+// @match        *://*.youtube.com/*
+// @match        *://*.discord.com/*
+// @match        *://nicuse.xyz/*
+// @match        *://404063.xyz/*
+// @match        *://link4m.com/go/*
+// @match        *://zks1.blogspot.com/?post=*
+// @match        *://zks2.blogspot.com/?post=*
+// @match        *://tutwuri.id/*
+// @match        *://sfl.gl/ready/go?u=*
+// @match        *://pastesio.com/*
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
 // ==/UserScript==
@@ -12,7 +101,7 @@
 (function() {
     'use strict';
 
-    const apiKey = '';
+    const apiKey = 'PASTE_YOUR_LINK_HERE';
     const serverUrl = 'https://raw.githubusercontent.com/perritoelpro32/NezBypass/refs/heads/main/url.txt';
 
     function getApiUrlFromGitHub() {
